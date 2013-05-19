e0bd1bb6dd5f70ae120145e69c1ad9841289b8cd
gabriel <gabriel@gabriel>
2013-05-19 21:46:47 +0200

    del title

--- index.php
+++ index.php
@@ -7,7 +7,6 @@
 	<link href="style.css" rel="stylesheet" />
 </head> 
 <body>
-	<h1>random picture collection</h1>
 	<div id="anim">
 		<img src="img/mini0.jpg" alt="" /><img src="img/mini1.jpg" alt="" /><img src="img/mini2.jpg" alt="" /><img src="img/mini3.jpg" alt="" /><img src="img/mini4.jpg" alt="" /><img src="img/mini5.jpg" alt="" />
 	</div>
